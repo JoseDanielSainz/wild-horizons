d0db8d672560a7d248399a2079e4467850e9760e
Jose Sainz <jose.sainz@clickbalance.com>
2025-08-13 11:19:18 -0700

    Added mock database, and some basic error handling

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 import http from 'node:http'
+import { getDataFromDB } from './database/db.js'
 
 // We need to specify the port
 const PORT = 8000
 
-const server = http.createServer((req, res) => {
-  // Sends data over http and ends the response
-  res.end('Hello from the server')
+
+const server = http.createServer(async (req, res) => {
+  const destinations = await getDataFromDB()
+
+  if (req.url === '/api' && req.method === 'GET') {
+    res.setHeader('Content-Type', 'application/json')
+    res.statusCode = 200
+    res.end(JSON.stringify(destinations))
+  } else {
+    res.setHeader('Content-Type', 'application/json')
+    res.statusCode = 404
+    res.end(JSON.stringify({ error: "not found", message: "The request route does not exist" }))
+  }
+
+
 })
 
 // We need to listen on that port, we need to also pass a callback to execute when the server connects
