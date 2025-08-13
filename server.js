fd9dceff27c09c602e5cebd355c0eae4d20cbcfe
Jose Sainz <jose.sainz@clickbalance.com>
2025-08-13 12:03:20 -0700

    Modularised the code with utility functions

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import http from 'node:http'
 import { getDataFromDB } from './database/db.js'
+import { sendJSONResponse } from './utils/sendJSONResponse.js'
+import { getDataByPathParams } from './utils/getDataByPathParams.js'
 
 // We need to specify the port
 const PORT = 8000
@@ -9,13 +11,29 @@ const server = http.createServer(async (req, res) => {
   const destinations = await getDataFromDB()
 
   if (req.url === '/api' && req.method === 'GET') {
-    res.setHeader('Content-Type', 'application/json')
-    res.statusCode = 200
-    res.end(JSON.stringify(destinations))
+    sendJSONResponse(res, 200, destinations)
+  } else if (req.url.startsWith('/api/continent') && req.method === 'GET') {
+
+    const continent = req.url.split('/').pop()
+    const filteredData = getDataByPathParams(destinations, 'continent', continent)
+
+    sendJSONResponse(res, 200, filteredData)
+
+  } else if (req.url.startsWith('/api/country') && req.method === 'GET') {
+
+    const country = req.url.split('/').pop()
+    const filteredData = getDataByPathParams(destinations, 'country', country)
+
+    sendJSONResponse(res, 200, filteredData)
+
+
   } else {
+
     res.setHeader('Content-Type', 'application/json')
-    res.statusCode = 404
-    res.end(JSON.stringify({ error: "not found", message: "The request route does not exist" }))
+    sendJSONResponse(res, 404, ({
+      error: 'not found',
+      message: 'The request route does not exist'
+    }))
   }
 
 
